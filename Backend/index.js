c35c1014c0295d6cf9a90d09037b596577d6b94f
jayeshk-star <jayeshkale052@gmail.com>
2021-10-19 13:01:57 +0530

    mongodb connection is successful

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,19 @@
-console.log("hello2")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 2345;
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.URL)
+  .then(() => console.log("data-base connection sucssful"))
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
