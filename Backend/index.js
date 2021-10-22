bc59a9606808a97b8bb957b961f6c659147770e2
jayeshk-star <jayeshkale052@gmail.com>
2021-10-22 20:23:55 +0530

    1st endpoing post data created

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const PORT = process.env.PORT || 2345;
 
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const userRoute=require("./routes/user")
 dotenv.config();
+app.use(express.json())
 
 mongoose
   .connect(process.env.URL)
-  .then(() => console.log("data-base connection sucssful"))
-  .catch((error) => {
-    console.log(error);
+  .then(() => {
+    console.log("connect succesful");
+  })
+  .catch((e) => {
+    console.log(e);
   });
+ 
+  app.use("/api/users",userRoute)
 
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+  console.log("backend is running");
 });
