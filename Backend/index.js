2f874593ff565d63908cfbc2ad27f42d6ac13dbc
jayeshk-star <jayeshkale052@gmail.com>
2021-10-27 23:49:04 +0530

    new routes added in server file

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,9 @@ const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000;
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 mongoose
@@ -20,6 +23,9 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
