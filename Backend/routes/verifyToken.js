4b49cf3d34aea68e34d3ebbb3d8a73d6f0be828d
jayeshk-star <jayeshkale052@gmail.com>
2021-10-27 23:48:09 +0530

    all models and routes working with postaman

--- Backend/routes/verifyToken.js
+++ Backend/routes/verifyToken.js
@@ -1,29 +1,41 @@
-const Jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const authH = req.headers.token;
-
-  if (authH) {
-      
-    Jwt.verify(token, process.env.JWTKEY, (err, user) => {
-      if (err) res.status(403).json("token not valid");
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWTKEY, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
       req.user = user;
       next();
     });
   } else {
-    return res.status(401).json("not login");
+    return res.status(401).json("You are not authenticated!");
   }
 };
 
-const verifyandlogin=(req,res,next)=>{
-    verifyToken(res,req,()=>{
-        if(req.user.id=== req.params.id || req.user.isAdmin){
-            next()
-        }else{
-            res.status(403).json("failed to do")
-        }
-    })
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
 
-}
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
 
-module.exports = { verifyToken,verifyandlogin };
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
