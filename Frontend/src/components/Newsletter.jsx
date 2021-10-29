9550be2aff09c0a30ee914d443d3c93c740b35f8
jayeshk-star <jayeshkale052@gmail.com>
2021-10-29 09:35:05 +0530

    add to cart done

--- Frontend/src/components/Newsletter.jsx
+++ Frontend/src/components/Newsletter.jsx
@@ -1,64 +1,65 @@
-import { Send } from "@material-ui/icons";
-import styled from "styled-components";
-import { mobile } from "../responsive";
+import { Send } from '@material-ui/icons'
+import styled from 'styled-components'
+import { mobile } from '../responsive'
 
-const Container = styled.div`
+const MainConatainer = styled.div`
   height: 60vh;
   background-color: #fcf5f5;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-`;
-const Title = styled.h1`
+`
+const TitleBar = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
-`;
+`
 
-const Desc = styled.div`
+const DescriptionBar = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
-  ${mobile({ textAlign: "center" })}
+  ${mobile({ textAlign: 'center' })}
+`
 
-`;
-
-const InputContainer = styled.div`
+const InputBar = styled.div`
   width: 50%;
   height: 40px;
   background-color: white;
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
-  ${mobile({ width: "80%" })}
-`;
+  ${mobile({ width: '80%' })}
+`
 
 const Input = styled.input`
   border: none;
   flex: 8;
   padding-left: 20px;
-`;
+`
 
 const Button = styled.button`
   flex: 1;
   border: none;
   background-color: teal;
   color: white;
-`;
+`
 
 const Newsletter = () => {
   return (
-    <Container>
-      <Title>Newsletter</Title>
-      <Desc>Get timely updates from your favorite products.</Desc>
-      <InputContainer>
-        <Input placeholder="Your email" />
+    <MainConatainer>
+      <TitleBar>Newsletter</TitleBar>
+      <DescriptionBar>
+        Get timely updates from your favorite products.
+      </DescriptionBar>
+      <InputBar>
+        <Input placeholder='Your email' />
         <Button>
           <Send />
         </Button>
-      </InputContainer>
-    </Container>
-  );
-};
+      </InputBar>
+    </MainConatainer>
+  )
+}
 
-export default Newsletter;
+export default Newsletter
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-// import { store, persistor } from "./redux/store";
-// import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
-  <Provider>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
-  </Provider>,
+  <>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      <App />
+      </PersistGate>
+    </Provider>
+  </>,
   document.getElementById("root")
 );
